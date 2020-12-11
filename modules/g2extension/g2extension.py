48cdd9d53fcd2756e4e61c0c6df01d88f324d756
abrente <71020969+abrente@users.noreply.github.com>
2020-12-11 17:09:13 +0100

    Update g2extension.py

--- modules/g2extension/g2extension.py
+++ modules/g2extension/g2extension.py
@@ -3,14 +3,8 @@ from amuse.couple.bridge import CalculateFieldForParticles
 
 class Gadget2Gravity(Gadget2): 
     """ Extension of Gadget2 with gravity included """ 
-    def init(self, 
-             converter, 
-             #mode='normal', 
-             **kwargs): 
-        Gadget2.__init__(self, 
-                         converter, 
-                         #mode=mode, 
-                         **kwargs) 
+    def init(self, converter, **kwargs): 
+        Gadget2.__init__(self, converter, **kwargs) 
         
     def get_gravity_at_point(self, radius, x, y, z): 
         field_code = CalculateFieldForParticles(particles=self.dm_particles) 
